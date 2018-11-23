436bea2d246c601b71cc7981e344ba4e9e06dcaa
CaiKnife <caiknife@foxmail.com>
2018-11-23 13:49:40 +0800

    if & switch

---
+++ if.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func max(a int, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a int, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func sign(a int) int {
+	if a > 0 {
+		return 1
+	} else if a < 0 {
+		return -1
+	} else {
+		return 0
+	}
+}
+
+func main() {
+	fmt.Println(sign(max(min(24, 42), max(24, 42))))
+	fmt.Println(sign(max(min(24, 42), max(24, 42))))
+}
