4a2e08ff69e9c94cc91c76d7596f62a9a607e46e
CaiKnife <caiknife@foxmail.com>
2018-11-29 11:42:11 +0800

    map

--- map.go
+++ map.go
@@ -12,4 +12,28 @@ func main() {
 		60: "及格", // 注意这里逗号不可缺少，否则会报语法错误
 	}
 	fmt.Println(n, len(n))
+
+	var aMap = map[string]int{
+		"one":   1,
+		"two":   2,
+		"three": 3,
+	}
+
+	k := "two"
+
+	v, ok := aMap[k]
+
+	if ok {
+		fmt.Printf("The element of key %q: %d\n", k, v)
+	} else {
+		fmt.Println("Not found!")
+	}
+
+	for k, v := range aMap {
+		fmt.Printf("The element of key %q: %d\n", k, v)
+	}
+
+	bMap := map[string]int{}
+
+	fmt.Println(bMap)
 }
